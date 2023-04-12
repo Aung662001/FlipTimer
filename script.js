8195777aa733ea469e5e2d2591212c808b0b1949
Aung662001 <107274438+Aung662001@users.noreply.github.com>
2023-04-12 10:57:43 +0630

    finsished

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
 const flipCard = document.querySelector(".flip-card");
 
-const targetDate = new Date().setHours(new Date().getHours() + 1);
-let remaningTimeInterval;
+const targetDate = new Date().setHours(new Date().getHours() + 24);
 setInterval(() => {
   let currentDate = new Date();
   remainTime = Math.ceil((targetDate - currentDate) / 1000);
-  if (remaningTimeInterval !== remainTime) {
-    flipAllCard(remaningTimeInterval);
-  }
-  remaningTimeInterval = remainTime;
+  flipAllCard(remainTime);
 }, 200);
 function flipAllCard(time) {
-  let seconds = time % 60;
+  let seconds = time % 60; //second between 0 and 59
   let minutes = Math.floor((time / 60) % 60);
   let hours = Math.floor(time / 3600);
-  console.log(seconds, minutes, hours);
+
+  flip(document.querySelector("[data-hours-ten]"), Math.floor(hours / 10));
+  flip(document.querySelector("[data-hours-one]"), hours % 10);
+
+  flip(document.querySelector("[data-minutes-ten]"), Math.floor(minutes / 10));
+  flip(document.querySelector("[data-minutes-one]"), minutes % 10);
+
+  flip(document.querySelector("[data-seconds-ten]"), Math.floor(seconds / 10));
+  flip(document.querySelector("[data-seconds-one]"), seconds % 10);
 }
-function flip() {
-  const topHalf = document.querySelector(".top");
-  const bottomHalf = document.querySelector(".bottom");
+function flip(element, realFlipTime) {
+  const topHalf = element.querySelector(".top");
+  const bottomHalf = element.querySelector(".bottom");
   const topFlip = document.createElement("div");
   topFlip.classList.add("top-flip");
   const bottomFlip = document.createElement("div");
   bottomFlip.classList.add("bottom-flip");
   let startPoint = parseInt(topHalf.textContent);
-
+  if (startPoint === realFlipTime) return;
   bottomHalf.innerText = startPoint;
 
   topFlip.innerText = startPoint;
-  bottomFlip.innerText = startPoint - 1;
+  bottomFlip.innerText = realFlipTime;
 
   topFlip.addEventListener("animationstart", () => {
-    topHalf.innerText = startPoint - 1;
+    topHalf.innerText = realFlipTime;
   });
   topFlip.addEventListener("animationend", () => {
     topFlip.remove();
   });
   bottomFlip.addEventListener("animationend", () => {
-    bottomHalf.innerText = startPoint - 1;
+    bottomHalf.innerText = realFlipTime;
     bottomFlip.remove();
   });
 
-  flipCard.append(topFlip, bottomFlip);
+  element.append(topFlip, bottomFlip);
 }
